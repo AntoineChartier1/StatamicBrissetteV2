c48a1c870bfee7aa32c4c7b15b2d03d4f3eadd16
AntoineChartier1 <1363491@cstj.qc.ca>
2024-04-22 07:37:43 -0400

    lundi matin push

--- resources/js/site.js
+++ resources/js/site.js
@@ -3,11 +3,14 @@
 import { Carousel } from 'flowbite';
 
 const initApp = () => {
+    const body = document.body;
     const hamburgerBtn = document.getElementById("hamburger-button");
     const mobileMenu = document.getElementById("mobile-menu");
 
     const toggleMenu = () => {
         mobileMenu.classList.toggle("hidden");
+        body.classList.toggle("overflow-hidden"); // 
+        // body.style.overflow = 'hidden';
         // mobileMenu.classList.toggle("flex");
         hamburgerBtn.classList.toggle("toggle-btn");
     };
