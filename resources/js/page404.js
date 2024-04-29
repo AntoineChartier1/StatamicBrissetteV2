9d06a4259d73adfaf43bfb325c6ff623747bfc41
AntoineChartier1 <1363491@cstj.qc.ca>
2024-04-29 09:23:31 -0400

    ajout lenis

--- resources/js/page404.js
+++ resources/js/page404.js
@@ -1,25 +1,15 @@
+import Lenis from 'lenis'
 
 
+const lenis = new Lenis()
 
-// Wait for the page to load
-window.addEventListener('load', () => {
-    // After a short delay, open the storage unit
-    setTimeout(() => {
-        const storageUnit = document.getElementById('storage-unit');
-        storageUnit.classList.remove('storage-unit-closed');
-        storageUnit.classList.add('storage-unit-open');
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
 
-        // After the storage unit is open, reveal the 404 sign
-        const errorSign = document.getElementById('error-sign');
-        errorSign.classList.remove('hidden');
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
 
-        // Animate the 404 sign to catch the user's attention
-        gsap.to(errorSign, {
-            duration: 1,
-            scale: 1.5,
-            repeat: -1,
-            yoyo: true,
-            ease: 'power1.inOut'
-        });
-    }, 2000);
-});
+requestAnimationFrame(raf)
