9d2da568d53a40268ee3b465d9b78772ded06665
AntoineChartier1 <1363491@cstj.qc.ca>
2024-04-29 10:16:52 -0400

    smooth scrool everywhere

--- resources/js/page404.js
+++ resources/js/page404.js
@@ -7,9 +7,3 @@ lenis.on('scroll', (e) => {
   console.log(e)
 })
 
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,21 +1,24 @@
+import Lenis from 'lenis'
 // This is all you.
+
 
 const initApp = () => {
     const body = document.body;
     const hamburgerBtn = document.getElementById("hamburger-button");
     const mobileMenu = document.getElementById("mobile-menu");
 
-const animationDuration = window.getComputedStyle(mobileMenu).animationDuration;
+    const animationDuration =
+        window.getComputedStyle(mobileMenu).animationDuration;
 
-// Remove 's' or 'ms' from the end of the string and convert to a number
-let duration = parseFloat(animationDuration);
+    // Remove 's' or 'ms' from the end of the string and convert to a number
+    let duration = parseFloat(animationDuration);
 
-// If the original duration was in seconds, convert to milliseconds
-if (animationDuration.includes('s')) {
-    duration *= 1000;
-}
+    // If the original duration was in seconds, convert to milliseconds
+    if (animationDuration.includes("s")) {
+        duration *= 1000;
+    }
 
-    mobileMenu.classList.remove('animate-close-menu', 'animate-open-menu');
+    mobileMenu.classList.remove("animate-close-menu", "animate-open-menu");
     let firstClick = true;
 
     const toggleMenu = () => {
@@ -48,7 +51,15 @@ if (animationDuration.includes('s')) {
 
     hamburgerBtn.addEventListener("click", toggleMenu);
 
+    // smooth scroll
+    const lenis = new Lenis();
 
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
